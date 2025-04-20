0a348e748be30aa144c4de755eaad683872c08cf
Lazzi1337 <karlsen_lasse@hotmail.com>
2025-04-20 20:52:47 +0200

    Delete src/src/index.js

--- src/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import PrezCheckDemo from "./PrezCheckDemo";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<PrezCheckDemo />);
